d3cc2dc667be5925aadb123459055e19b6b2fb81
Paul Crane <paul@acheronsoft.com>
2017-08-02 01:38:11 -0500

    Add removeLines function that takes a predicate.

--- source/dfileutils/file.d
+++ source/dfileutils/file.d
@@ -167,11 +167,47 @@ void removeLines(const string fileName, const ulong startLine, const ulong remov
 	}
 }
 
+void removeLines(alias predicate)(const string fileName, const ulong startLine, const ulong removeLine)
+{
+	if(fileName.exists)
+	{
+		immutable auto lines = readText(fileName).splitLines();
+
+		if(lines.length < startLine + removeLine)
+		{
+			throw new Exception("Can't delete lines past the end of file!");
+		}
+		else
+		{
+			auto f = File(fileName, "w");
+
+			foreach (ulong currentLine, line; lines)
+			{
+				if (startLine > currentLine || currentLine >= startLine + removeLine)
+				{
+					if(predicate(currentLine))
+					{
+						f.writeln(line);
+					}
+				}
+			}
+		}
+	}
+	else
+	{
+		throw new FileException("File not found!");
+	}
+}
+
 void removeLine(const string fileName, const string lineText)
 {
 }
 
 void removeLines(const string fileName, const string lineText)
+{
+}
+
+void removeLines(alias predicate)(const string fileName, const string lineText)
 {
 }
 
