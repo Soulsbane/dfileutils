11e8a70bbf59cd1d3637c617385f8539e9b81c16
Paul Crane <paul@acheronsoft.com>
2017-10-11 22:38:52 -0500

    Initial commit of mmreadtext module.

---
+++ source/dfileutils/mmreadtext.d
@@ -0,0 +1,61 @@
+module dfileutils.mmreadtext;
+
+import std.range : array, front;
+import std.algorithm : min;
+import std.mmfile : MmFile;
+import std.utf : decodeFront;
+
+@safe:
+struct MmText {
+public:
+	this(string filename) @trusted
+	{
+		file_ = new MmFile(filename);
+	}
+
+	dchar front()
+	{
+		auto data = nextSlice;
+		return data.decodeFront;
+	}
+
+	void popFront()
+	{
+		auto data = nextSlice;
+		size_t bytes;
+
+		data.decodeFront(bytes);
+		fileOffset_ += bytes;
+	}
+
+	bool empty()
+	{
+		return ulLength <= 0;
+	}
+
+private:
+	ulong ulLength() const @trusted
+	{
+		return file_.length - fileOffset_;
+	}
+
+	private string nextSlice() @property @trusted
+	{
+		auto offset = min(4, ulLength);
+		return cast(string)file_[fileOffset_ .. fileOffset_ + offset];
+	}
+
+private:
+	ulong  fileOffset_;
+	MmFile file_;
+}
+
+unittest
+{
+	import std.algorithm.comparison : equal;
+
+	/*auto expected = import(__FILE_FULL_PATH__);
+	auto result = MmText(__FILE_FULL_PATH__);
+
+	assert(equal(result, expected));*/
+}
--- source/dfileutils/package.d
+++ source/dfileutils/package.d
@@ -4,5 +4,6 @@ public
 {
 	import dfileutils.extractor;
 	import dfileutils.file;
+	import dfileutils.mmreadtext;
 	import dfileutils.tempfile;
 }
