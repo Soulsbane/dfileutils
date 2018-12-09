8bf46e3046d0281ef7de9074d7826090649035d3
Paul Crane <paul@acheronsoft.com>
2018-12-08 19:14:40 -0600

    Add initial AutoLockFile struct.

--- source/dfileutils/autolock.d
+++ source/dfileutils/autolock.d
@@ -2,3 +2,42 @@
 	Automatically locks a file before reading/writing to it.
 */
 module dfileutils.autolock;
+
+import std.stdio;
+
+struct AutoLockFile
+{
+	this(string fileName, string mode)
+	{
+		fileName_ = fileName;
+		fileHandle_ = File(fileName, mode);
+	}
+
+	bool writeln(S...)(S args)
+	{
+		bool locked = fileHandle_.tryLock();
+
+		if(locked)
+		{
+			fileHandle_.writeln(args);
+			fileHandle_.unlock();
+		}
+
+		return locked;
+	}
+
+	File getHandle()
+	{
+		return fileHandle_;
+	}
+
+private:
+	File fileHandle_;
+	string fileName_;
+}
+
+unittest
+{
+	auto f = AutoLockFile("test.txt", "w");
+	f.writeln("This is a test");
+}
